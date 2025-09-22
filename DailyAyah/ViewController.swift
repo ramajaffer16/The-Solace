bc0eb3d83ff0d2997dccead94330c74fa0c5113e
Ramadhan <ramadhan.ramadhan@equitybank.co.ke>
2025-09-22 15:24:06 +0300

    Initial Commit

---
+++ DailyAyah/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyAyah
+//
+//  Created by Ramadhan on 22/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
